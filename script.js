f75927e2afa82bc2d7376bc96ed925cb148ceafe
Lorena <lzuniga1315@gmail.com>
2022-02-23 17:49:56 -0800

    redid if statements

--- script.js
+++ script.js
@@ -2,70 +2,62 @@
 var generateBtn = document.querySelector("#generate");
 
 // generator numbers/symbols/letters
-var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
-function generatePassword(){
-  // window prompt for uppercase letter
-// passwordLength= 1-128
-// window.prompt("Uppercase Letters?")
+function generatePassword() {
+  var allCharacters = [];
+  var resultPassword = "";
+  var confirmLength = (prompt("what length would you like your password be?"));
 
-// window.prompt("Lowercase Letters")
+  if (confirmLength < 8 || confirmLength > 128) {
+    alert("Please make password between 8-128 characters.")
+    return;
+  }
 
-// window.prompt("numbers?")
+  if (confirmLength) {
+    if (confirm("want uppercase?")) {
+      allCharacters.push(alphaUpper);
+    }
+    if (confirm("want lowercase?")) {
+      allCharacters.push(alphaLower);
+    }
+    if (confirm("want number characters?")) {
+      allCharacters.push(numbers);
+    }
+    if (confirm("want symbolds?")) {
+      allCharacters.push(symbols);
+    }
+    if (allCharacters.length === 0) {
+      alert("Atleast one character must be selected");
+      return;
+    } else {
+      for (var i = 0; i < confirmLength; i++) {
+        var randomCharacter = Math.floor(Math.random() * allCharacters.length);
+        const selectedChar = allCharacters[randomCharacter];
+        var randomChar = Math.floor(Math.random() * selectedChar.length);
+        resultPassword += selectedChar[randomChar];
+      }
+    }
+  }
 
-// window.prompt("symbols?")
-
-
-
-
-// for (var i = 0; i <= passwordLength; i++) {
-//     var randomNumber = Math.floor(Math.random() * chars.length);
-//     password += chars.substring(randomNumber, randomNumber +1);
-//    }
-
-//first prompt should ask the length of password. min 8 max 128 characters
-
-var passwordLength= parseInt(prompt("what length should your password be?"))
-
-if(Number.isNaN(passwordLength)){
-  alert("This must be a number")
-  return null;
+  document.getElementById("password").textContent= resultPassword
+  return resultPassword;
 }
+  // if(Number.isNaN(passwordLength)){
+  //   alert("This must be a number")
+  //   return null;
+  // }
 
-if(passwordLength<8){
-alert("Min of 8 characters");
-return null;
-}
-if(passwordLength>128){
-alert("Max of 128 characters")
-return null;
-}
-
-var uppercase= confirm("Uppercase characters?")
-
-var lowercase= confirm("Lowercase characters?")
-var numbers=
-var symbols=
-
-//once the user confirms what they want in their password then push all the available characters into an empty array
-
-//create a forloop that pushes into a final array using the random function . 
-
-//return the joined array...look at .join('')
-
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+  // if(passwordLength<8){
+  // alert("Min of 8 characters");
+  // return null;
+  // }
+  // if(passwordLength>128){
+  // alert("Max of 128 characters")
+  // return null;
+  // }
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", generatePassword);
